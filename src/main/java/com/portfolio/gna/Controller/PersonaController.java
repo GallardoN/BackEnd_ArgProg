8b815a97198650705a563aff61afe8186fe082a3
GallardoN <nahuelgallardo2013@gmail.com>
2023-04-25 12:44:04 -0300

    Me faltó un / en el PersonaController

--- src/main/java/com/portfolio/gna/Controller/PersonaController.java
+++ src/main/java/com/portfolio/gna/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = {"https://frontendgna.web.app","http://localhost4200"})
+@CrossOrigin(origins = {"https://frontendgna.web.app/","http://localhost4200"})
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
